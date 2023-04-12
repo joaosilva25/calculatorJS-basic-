6390f6166ed6a5f0da3c389665b6a86cc1397b94
unknown <joaosilva2505@outlook.com>
2023-04-12 15:59:58 -0300

    (primeiro commit)

---
+++ code.js
@@ -0,0 +1,31 @@
+function clicou() {
+    let num1=Number(prompt('primeiro numero:'));
+    let num2=Number(prompt('segundo numero:'));
+    let op= ['1.soma','2.subtracao','3.divisao','4.multiplicação'];
+    let saida= Number(prompt (`Valores informados: ${num1} e ${num2}\nO que vamos fazer?\n
+${op[0]} \n ${op[1]} \n ${op[2]} \n ${op[3]}`));
+
+
+
+let text= document.getElementById('text')
+text.innerHTML = `<h2>Calculando...</h2>`
+
+switch (saida) {
+    case 1:
+        text.innerHTML += `<p>${num1}+${num2}=${num1+num2}</p>`
+        break
+    case 2:
+        text.innerHTML += `<p>${num1}-${num2}=${num1-num2}</p>`
+        break
+    case 3:
+        text.innerHTML += `<p>${num1}/${num2}=${num1/num2}</p>`
+        break
+    case 4:
+        text.innerHTML += `<p>${num1}*${num2}=${num1*num2}</p>`
+        break
+    default:
+        text.innerHTML += `<p>OPÇÃO INVÁLIDA! Você digitou ${num1} e ${num2}, mas não sei o que fazer com eles. </p>`
+         break
+}
+
+}
